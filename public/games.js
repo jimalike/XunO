ba441d9cb8ab7a966aeec11a1618823d4102af44
Supanut <51752762+jimalike@users.noreply.github.com>
2022-04-03 20:24:44 +0700

    update create room

--- public/games.js
+++ public/games.js
@@ -12,23 +12,32 @@ function makeid(length) {
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
+    ref.child('Roomlist').update({
+        Room: result
+    });
     return result;
 }
+
+// firebase.auth().onAuthStateChange((user) => {
+//     setupUI(user);
+// })
+// ref.child('Roomnumber').writeUserData({
+//     Roomnumber: params.id,
+// });
 
 function Create() {
     // console.log("โหล")
     window.location.href = `game.html?id=${makeid(10)}`
+
 }
 
-
+// const currentUser = firebase.auth().currentUser;
+//     console.log('[Join] Current user:', currentUser)
 
 const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop), })
 console.log(params.id);
 
 
-ref.child('Roomnumber').writeUserData({
-    Roomnumber: params.id,
-});
 
 ref.on('value', snapshot => {
 
